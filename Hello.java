b77d8d3855af3d94b9464c19bd766663302e9ded
JustinEckard <justinweckard@gmail.com>
2024-07-31 23:22:11 +0200

    Chapter 1: figuring out the weird stuff with datatypes in java

---
+++ Hello.java
@@ -0,0 +1,32 @@
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        chapter1();
+    }
+
+    public static void chapter1(){
+        System.out.println("Chapter 1 rudimentary Java\n");
+
+        /* 
+        byte a, b = 1 , c = 2;
+        a = (byte) (b + c);  // You need to cast this because the + operator returns a integer not a byte
+        System.out.println(a);  // This will now return 3
+
+        // char ch = '\u13ab';  // Will return a question mark as not all systems support this system of unicode characters
+        char ch = '\123';  // ASCII for capital S , using the \ followed by the ASCII code will return the ASCII value too
+        System.out.println(ch); 
+
+        // int i = 0123; // Prefizing with a 0 will provide the octal 0-7 of the code
+        int j = 0x12a3; // Prefixing with 0x is the same but for the hexidecimal values 0-9 A-E
+        System.out.println(j);
+
+        // float f = 123.00100;  // This will give an error as you cannot interchange double and float
+
+        float f = 123.0034f; // You need to add the f as a suffix modifier this can also be F
+        System.out.println(f);
+        */
+    }
+}
